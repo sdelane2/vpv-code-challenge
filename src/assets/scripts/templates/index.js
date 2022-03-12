bf80fb240d6ac2dc40ef5c2a60b0257d6c792fa5
sdelane2 <seanmartindelaney@gmail.com>
2022-03-12 16:39:58 -0500

    Start showing some product data on the page

--- src/assets/scripts/templates/index.js
+++ src/assets/scripts/templates/index.js
@@ -2,7 +2,15 @@ import '../theme';
 import '../../styles/templates/index.scss';
 import getAllProducts from '../graphql/collection-starter-code';
 
+const container = document.querySelector('.card-container')
+
 getAllProducts('test-collection').then(data => {
     console.log(data)
+    data.forEach(product => {
+        container.insertAdjacentHTML("beforeend", 
+        `
+            <div>${product.title}</div>   
+        `
+        )  
+    });  
 })
-
