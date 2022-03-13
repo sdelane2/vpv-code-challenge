8182f1e1c2bfd65f73647a10abdce6ef26f5d791
sdelane2 <seanmartindelaney@gmail.com>
2022-03-12 19:42:47 -0500

    Product images with card and swatches on CLP

--- src/assets/scripts/templates/index.js
+++ src/assets/scripts/templates/index.js
@@ -4,36 +4,40 @@ import getAllProducts from '../graphql/collection-starter-code';
 
 const container = document.querySelector('.card-container')
 
-const swatches = (product) => { 
-    if(product.options[0]['name'] == 'Color'){
-        product.options[0].values.map(name => {
-            let colorClass 
-            if(name === 'Blue') colorClass = "blue"
-            else if(name === 'Red') colorClass = "red"
-            else if(name === 'Gold') colorClass = "gold"
-            else if(name === 'Brown') colorClass = "brown"
-            else if(name === 'Medium Grey') colorClass = "mediumgray"
-            else if(name === 'Navy Blue') colorClass = 'navy'
-            else if(name === 'Navy') colorClass = "navy"
-            else if(name === 'Yellow') colorClass = "yellow"
-            else if(name === 'Dark Wash') colorClass = "darkwash"
-            else if(name === 'Light Wash') colorClass = "lightwash"
-            container.insertAdjacentHTML("beforeend",
-                `
-                    <a class="swatch ${colorClass}"></a>
-                ` 
-            )
-        })
-    }
-}
 getAllProducts('test-collection').then(data => {
     console.log(data)
     data.forEach(product => {
-        swatches(product)
-        container.insertAdjacentHTML("beforeend", 
+        // swatches(product)
+        
+        container.insertAdjacentHTML("afterbegin", 
             `
-                <div>${product.title}</div>
+                <div class= "card">
+                    <div>
+                        <img  src=${product.variants[0].image.src}/>
+                    </div>
+
+                    <div class="card__title">${product.title}</div>
+                    <div class="swatch-container">
+                    ${product.options[0]['name'] == 'Color' ?
+                        product.options[0].values.map(name => {
+                            let colorClass 
+                            if(name === 'Blue') colorClass = "blue"
+                            else if(name === 'Red') colorClass = "red"
+                            else if(name === 'Gold') colorClass = "gold"
+                            else if(name === 'Brown') colorClass = "brown"
+                            else if(name === 'Medium Grey') colorClass = "mediumgray"
+                            else if(name === 'Navy Blue') colorClass = 'navy'
+                            else if(name === 'Navy') colorClass = "navy"
+                            else if(name === 'Yellow') colorClass = "yellow"
+                            else if(name === 'Dark Wash') colorClass = "darkwash"
+                            else if(name === 'Light Wash') colorClass = "lightwash"
+                            return `<a class='swatch ${colorClass}'></a>`
+                        }).join(" ")
+                    :
+                    ''
+                }
+                </div>
             `
         ) 
-    });
+    });  
 })
