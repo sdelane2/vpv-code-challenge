fd85bb6ed45d17855233f0bc25545877b68e318a
sdelane2 <seanmartindelaney@gmail.com>
2022-03-13 19:15:50 -0400

    Cleaned up class names and added price functionality plus breakpoints

--- src/assets/scripts/templates/index.js
+++ src/assets/scripts/templates/index.js
@@ -20,25 +20,46 @@ function addCustomEventListener(selector, event, handler) {
 }
 
 function swatchClickHandler(e){
-    let closestImage = e.target.closest('.card').querySelector('.image-class')
+    let closestImage = e.target.closest('.card').querySelector('.card__image img')
+    let closestPrice = e.target.closest('.card').querySelector('.price')
+    console.log(e.target)
+    e.target.classList.add('active')
     closestImage.src = e.target.dataset.image
+    closestPrice.innerHTML = `$${e.target.dataset.price}`
 }
 addCustomEventListener('.swatch','click', swatchClickHandler);
 
 getAllProducts('test-collection').then(data => {
     data.forEach(product => {
+        
         let productObj = {
             title : product.title,
-            colorOptions: product.options[0].values, 
-            images : product.variants.map(variant => variant.image.src),
+            colorOptions: product.options[0].values,
+            variants : product.variants.map(variant => variant), 
+            images : [...new Set(product.variants.map(variant => variant.image.src))],
         }
-        productObj.images = [...new Set(productObj.images)]
+
+        let priceArray = []
+        let compareAtArray = []
+        productObj.variants.map(variant => {
+            productObj.images.map((image, index) => {
+                if(variant.image.src === image){
+                    priceArray[index] = variant.price
+                    compareAtArray[index] = variant.compareAtPrice
+                }
+            })
+        })
+        productObj['prices'] = priceArray
+        productObj['compareAtPrices'] = compareAtArray
+        
+        console.log(productObj)
         container.insertAdjacentHTML("afterbegin", 
             `
-                <div class= "card">
-                    <img class="image-class" src=${productObj.images[0]}/>
+                <div class="card">
+                    <div class="card__image"><img src=${productObj.images[0]}/></div>
                     <div class="card__title">${product.title}</div>
-                    <div class="swatch-container">
+                    <div class="card__price">${productObj['compareAtPrices'][0] !== null ? `<span class="price-compare">$${productObj['compareAtPrices'][0]}</span>` : '' }<span class="price">$${productObj['prices'][0]}</span></div>
+                    <div class="card__swatches">
                     ${product.options[0]['name'] == 'Color' ?
                         product.options[0].values.map((name, index) => {
                             let colorClass 
@@ -52,7 +73,7 @@ getAllProducts('test-collection').then(data => {
                             else if(name === 'Yellow') colorClass = "yellow"
                             else if(name === 'Dark Wash') colorClass = "darkwash"
                             else if(name === 'Light Wash') colorClass = "lightwash"
-                            return `<a class='swatch ${colorClass}' data-image='${productObj.images[index]}'  ></a>`
+                            return `<a class='swatch ${colorClass}' data-image='${productObj.images[index]}' data-price='${productObj['prices'][index]}' data-compareprice='${productObj['compareAtPrices'][index]}'  ></a>`
                         }).join(" ")
                     :
                     ''
